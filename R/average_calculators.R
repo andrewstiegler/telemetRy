025659177a79e7638c38bafcf6b7529aca246e19
Andrew Stiegler <andrew.stiegler@gmail.com>
2021-04-30 13:12:58 -0400

    fixed average for long dataframes

--- R/average_calculators.R
+++ R/average_calculators.R
@@ -24,11 +24,12 @@ arb_time_average <- function(data, avg_minutes) {
 
     df_classes <- sapply(data, class)
     ITime_check <- sum(df_classes == "ITime")
+    difftime_check <- sum(df_classes == "difftime")
     df_is_posixt <- sapply(data, is.POSIXct)
 
-    if (ITime_check == 0) {
+    if (ITime_check == 0 & difftime_check == 0) {
         if (sum(sapply(data, is.POSIXct)) == 0){
-            stop("Must contain a column of ITime or POSIXct")
+            stop("Must contain a column of ITime or POSIXct or difftime")
         }
     }
 
@@ -46,6 +47,13 @@ arb_time_average <- function(data, avg_minutes) {
                                 select(which(df_classes == "ITime")[1]))[2] -
                                 (data %>%
                                 select(which(df_classes == "ITime")[1]))[1])
+    }
+
+    if (ITime_check == 0 & difftime_check == 1) {
+        time_delta <- as.numeric((data %>%
+                                      select(which(df_classes == "difftime")[1]))[2] -
+                                     (data %>%
+                                          select(which(df_classes == "difftime")[1]))[1])
     }
 
     if (ITime_check == 0 & sum(sapply(data, is.POSIXct)) == 1) {
@@ -69,8 +77,13 @@ arb_time_average <- function(data, avg_minutes) {
         data <- data %>% select(-as.numeric(which(df_is_posixt))[1])
     }
 
-    for (rows_avg_iterator in 1:(nrow(data)/rows_per_avg)) {
-        data[rows_avg_iterator,] <- data %>%
+
+    data_num <- dplyr::select_if(data, is.numeric)
+    data_non_num <- dplyr::select_if(data, Negate(is.numeric))
+    data_non_num_avg <- data_non_num[seq(1, nrow(data_non_num), rows_per_avg),]
+    data_num_avg <- data_num[1:(nrow(data_num)/rows_per_avg)]
+    for (rows_avg_iterator in 1:(nrow(data_num)/rows_per_avg)) {
+        data_num_avg[rows_avg_iterator,] <- data_num %>%
             slice((rows_avg_iterator*rows_per_avg-(rows_per_avg-1)):
                       (rows_avg_iterator*rows_per_avg)) %>%
             colMeans(na.rm=TRUE) %>%
@@ -79,15 +92,28 @@ arb_time_average <- function(data, avg_minutes) {
             spread(key=.data$ind, value=.data$values)
         rows_avg_iterator <- rows_avg_iterator+1
     }
+    data <- cbind(data_non_num_avg, data_num_avg)
 
-    for (rows_avg_iterator in 1:(nrow(data)/rows_per_avg)) {
-        time_only <- data %>% select(as.numeric(which(df_is_posixt))[1])
-        time_only <- time_only %>%
-            slice((rows_avg_iterator*rows_per_avg-(rows_per_avg-1)):
-                            (rows_avg_iterator*rows_per_avg)) %>%
-            colMeans(na.rm=TRUE)
-    }
+#
+#     for (rows_avg_iterator in 1:(nrow(data)/rows_per_avg)) {
+#         data[rows_avg_iterator,] <- data %>%
+#             slice((rows_avg_iterator*rows_per_avg-(rows_per_avg-1)):
+#                       (rows_avg_iterator*rows_per_avg)) %>%
+#             colMeans(na.rm=TRUE) %>%
+#             stack() %>%
+#             as_tibble() %>%
+#             spread(key=.data$ind, value=.data$values)
+#         rows_avg_iterator <- rows_avg_iterator+1
+#     }
+#
+#     for (rows_avg_iterator in 1:(nrow(data)/rows_per_avg)) {
+#         time_only <- data %>% select(as.numeric(which(df_is_posixt))[1])
+#         time_only <- time_only %>%
+#             slice((rows_avg_iterator*rows_per_avg-(rows_per_avg-1)):
+#                             (rows_avg_iterator*rows_per_avg)) %>%
+#             colMeans(na.rm=TRUE)
+#     }
 
-    data <- head(data, nrow(data)/rows_per_avg)
+#    data <- head(data, nrow(data)/rows_per_avg)
     return(data)
 }
